b7b90c06248b81f91f64a00f36332c66fe2a75fd
Yowandaradillaa <yowandaradilla13@gmail.com>
2025-11-06 16:37:48 +0700

    menambahkan UI untuk menampilkan setiap item dalam items.

--- app/src/main/java/com/example/navigasiku/view/TampilData.kt
+++ app/src/main/java/com/example/navigasiku/view/TampilData.kt
@@ -14,6 +14,8 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -49,7 +51,21 @@ fun TampilData(
                 verticalArrangement = Arrangement.SpaceBy(
                     dimensionResource(id = R.dimen.padding_small)
                 )
-            )
+            ){
+                items.forEach { items ->
+                    Column {
+                        Text(
+                            text - item.first.uppercase(),
+                            fontSize = 16.sp
+                        )
+                        Text(
+                            text = item.second,
+                            fontWeight = FontWeight.Bold,
+                            fontFamily = FontFamily cursive fontSize = 22.sp
+                        )
+                    }
+                }
+            }
         }
     }
 }
