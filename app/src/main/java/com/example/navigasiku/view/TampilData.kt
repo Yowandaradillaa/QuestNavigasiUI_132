48be2426e6591efb7ec795558612dd534e5812e3
Yowandaradillaa <yowandaradilla13@gmail.com>
2025-11-06 16:30:13 +0700

    menambahkan scaffold

--- app/src/main/java/com/example/navigasiku/view/TampilData.kt
+++ app/src/main/java/com/example/navigasiku/view/TampilData.kt
@@ -1,18 +1,37 @@
 package com.example.navigasiku.view
 
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TampilData(
     OnBackBtnClick: () -> Unit
-){
+) {
     val items = listOf(
-        Pair (stringResource(id = R.string.nama_lengkap), "Contoh Nama"),
-        Pair (stringResource(id = R.string.jenis_kelamin), "Lainnya"),
-        Pair (stringResource(id = "Alamat"), "Yogyakarta")
+        Pair(stringResource(id = R.string.nama_lengkap), "Contoh Nama"),
+        Pair(stringResource(id = R.string.jenis_kelamin), "Lainnya"),
+        Pair(stringResource(id = "Alamat"), "Yogyakarta")
     )
+    Scaffold(
+        modifier = Modifier,
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = stringResource(id = R.string.tampil),
+                        color = Color.White
+                    )
+                },
+                colors = TopAppBarDefaults
+                    .MediumTopAppBarColors(containerColor = colorResource(id = R.color.teal_700))
+            )
+        })
 }
