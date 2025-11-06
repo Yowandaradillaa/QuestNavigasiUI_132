3e99bef4fae5bb50c2451bdd4615c553bbcf67db
Yowandaradillaa <yowandaradilla13@gmail.com>
2025-11-06 16:00:55 +0700

    memperbaiki error dan menambahkan row

--- app/src/main/java/com/example/navigasiku/view/TampilData.kt
+++ app/src/main/java/com/example/navigasiku/view/TampilData.kt
@@ -1,49 +1,76 @@
 package com.example.navigasiku.view
 
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.material3.HorizontalDivider
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.example.navigasiku.R
 
 @Composable
 fun FormIsian(
-    jenisK:List<String> = listOf("Laki-Laki", "Perempuan"),
-    OnSubmitBtnClick : () -> Unit
-){
-    Scaffold (modifier = Modifier,
+    jenisK: List<String> = listOf("Laki-Laki", "Perempuan"),
+    OnSubmitBtnClick: () -> Unit
+) {
+    Scaffold(
+        modifier = Modifier,
         topBar = {
             TopAppBar(
-                title = {Text( = stringResource(id = R.string.home),
-                    color = Color.White)},
-                colors = TopAppBarDefaults.topAppBarColors
-                    (containerColor = colorResource(id = R.color.teal_700))
+                title = {
+                    Text(
+                        text = stringResource(id = R.string.home),
+                        color = Color.White
+                    )
+                },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = colorResource(id = R.color.teal_700)
+                )
             )
         }
-    ){ isiRuang ->
-        Column(modifier = Modifier.padding(paddingValues = isiRuang),
+    ) { isiRuang ->
+
+        Column(
+            modifier = Modifier.padding(isiRuang),
             verticalArrangement = Arrangement.Top,
             horizontalAlignment = Alignment.CenterHorizontally
+        ) {
 
-        ){
-            OutlinedTextField (
-                value = ""
-                singleline = true,
+            OutlinedTextField(
+                value = "",
+                singleLine = true,
                 modifier = Modifier
                     .padding(top = 20.dp)
-                    .width(width = 250.dp),
+                    .width(250.dp),
                 label = { Text(text = "Nama Lengkap") },
                 onValueChange = {}
             )
-            HorizontalDivider(modifier = Modifier
 
+            HorizontalDivider(
+                modifier = Modifier
+                    .padding(20.dp)
+                    .width(250.dp),
+                thickness = 1.dp,
+                color = Color.Red
             )
-        }
 
+            Row {
+                jenisK.forEach { item ->
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                        modifier = Modifier.padding(end = 10.dp)
+                    ) {
+                        RadioButton(
+                            selected = false,
+                            onClick = {item}
+                        )
+                        Text(text = item)
+                    }
+                }
+            }
+        }
     }
 }
